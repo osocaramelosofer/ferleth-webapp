487325a22e03d6ce29ba8706cf68202d0fbf3555
Fernando Miguel <miguel@itnora.com>
2023-07-18 21:19:26 -0600

    wip data table

--- pages/order/view.vue
+++ pages/order/view.vue
@@ -1,16 +1,78 @@
 <script setup lang="ts">
-import { NSpace, NDataTable } from "naive-ui"
-
+// naive ui imports
+import { h, defineComponent } from 'vue'
+import { NSpace, NDataTable,useMessage, DataTableColumns, NButton } from "naive-ui"
+// firebase imports
 import { collection, getDocs } from "firebase/firestore";
 import {db} from "@/firebase"
+// Local imports
+import {Order} from "@/types/Order"
 
 const querySnapshot = await getDocs(collection(db, "orders"));
+// const orders: Order[] = [] todo: do with type Order
+// todo: create an interface extends from Order that will be OrderReadOnly and just add the id, this to use it only when we read data
+let ordersData = []
+
 querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${ JSON.stringify( doc.data() ) }`);
+  const orderData = doc.data();
+  const order = { ...orderData };
+  // orders.push(order);
+  ordersData = [...ordersData, order]
+
+  // todo: do with type Order
+  // const order: Order = {
+  //   // Assign the properties from the orderData object
+  //   // to the new order object
+  //   // Example:
+  //   id: orderData.id,
+  //   // Assign other properties similarly
+  // };
+
 });
 
 
-
+const createColumns = ({play}: { play: (row: Order) => void }): DataTableColumns<Order> => {
+  return [
+    {
+      title: 'Order Number',
+      key: 'orderNumber'
+    },
+    {
+      title: 'Name',
+      key: 'schoolName'
+    },
+    {
+      title: 'Stole Pieces',
+      key: 'stole.pieces'
+    },
+    {
+      title: 'Stole Color',
+      key: 'stole.color'
+    },
+    {
+      title: 'Action',
+      key: 'actions',
+      render (row) {
+        return h(
+            NButton,
+            {
+              strong: true,
+              tertiary: true,
+              size: 'small',
+              onClick: () => play(row)
+            },
+            { default: () => 'Play' }
+        )
+      }
+    }
+  ]
+}
+const message = useMessage()
+const columns2 = createColumns({
+  play (row: Order) {
+    message.info(`Play ${row.schoolName}`)
+  }
+})
 const columns = [
   {
     title: 'Name',
@@ -27,7 +89,7 @@ const columns = [
 ]
 
 const data = Array.from({ length: 46 }).map((_, index) => ({
-  key: index,
+  // key: index+1,
   name: `Edward King ${index}`,
   age: 32,
   address: `London, Park Lane no. ${index}`
@@ -46,15 +108,18 @@ const pagination = reactive({
     pagination.page = 1
   }
 })
-console.log("Query snapshot=>",querySnapshot)
+
 </script>
 
 <template>
   <div>
     <h1>view orders</h1>
-    <n-data-table :columns="columns" :data="data" :pagination="pagination" />
+    <n-data-table :columns="columns2" :data="ordersData" :pagination="pagination" />
 
-
+    <n-space>
+      <pre>{{ordersData}}</pre>
+      <pre>{{data}}</pre>
+    </n-space>
   </div>
 </template>
 <style scoped>
