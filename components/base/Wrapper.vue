44243536fde0ae5305ad9584b93db2740b135620
Fernando Miguel <miguel@itnora.com>
2023-07-20 21:53:01 -0600

    dark mode setup

--- components/base/Wrapper.vue
+++ components/base/Wrapper.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="px-16 bg-slate-100 md:px-44 h-full ">
+  <div class="px-16 bg-slate-100 md:px-44 h-full dark:bg-b">
     <div flex justify-center w-full p-4 >
       <slot />
     </div>
--- uno.config.ts
+++ uno.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
       'veryCool': '#0bd8a2', // class="text-very-cool"
       'valiente': '#EBEDD8',
       'valiente-second': '#FFB41D',
+      // 'b': '#1e1e20',original black
+      // 'b': '#010d1a', first one blue dark
+      'b': 'rgba(1, 31, 61, 1)',
+      'w': 'rgba(255,255,245,0.86)',
 
     }
   },
