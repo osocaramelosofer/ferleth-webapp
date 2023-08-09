6e37305dc90347ac38641fcb21c79f87eb453bec
Fernando Miguel <miguel@itnora.com>
2023-08-08 23:17:53 -0600

    create i18n constants

---
+++ constants/locales/orderFormLocales.js
@@ -0,0 +1,29 @@
+// Dictionary to internationalize the app
+
+export const orderFormLocales = {
+  'es-AR': {
+    titleForm: 'Crear Orden',
+    schoolName: 'Nombre del colegio',
+    schoolNamePlaceholder: 'Introduzca el nombre del colegio',
+    timestampCreation: "Fecha de Creación",
+    dueTimestamp: "Fecha de Entrega",
+    numberOfPieces:"Número de Piezas",
+    quality:"Calidad",
+    totalCost: "Costo Total",
+    next:'Siguiente',
+
+  },
+  'en-US': {
+    titleForm: 'Create Order',
+    defaultTheme: 'Default Theme',
+    schoolName: 'Collage Name',
+    schoolNamePlaceholder: 'Enter the name of the collage',
+    timestampCreation: "Date of Order Creation",
+    dueTimestamp: "Due Date",
+    numberOfPieces:"Number of Pieces",
+    quality:"Quality",
+    totalCost: "Total Cost",
+    next:'Next',
+
+  }
+}
