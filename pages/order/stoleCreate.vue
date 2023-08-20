66263b76e893a23537d8b7a3b387f96f91836c38
Fernando Miguel <miguel@itnora.com>
2023-08-20 15:05:27 -0600

    feature upload stole image to the storage

--- pages/order/stoleCreate.vue
+++ pages/order/stoleCreate.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { NForm, NFormItem, NInput, NButton, NSelect,FormInst, useMessage, NSpace, NModal, NIcon,
   NTooltip, NCard, NUpload, UploadFileInfo, UploadInst   } from 'naive-ui'
-import {InformationCircleSharp, HelpCircle} from '@vicons/ionicons5'
+import {InformationCircleSharp, HelpCircle, ArrowBack, CloudDownloadOutline} from '@vicons/ionicons5'
 import { stoleColorOptions, stoleTypeOptions } from "@/helpers/order/stole"
 import { stoleFormRules } from "~/helpers/order/orderFormRules"
 import { ref, watchEffect } from "vue";
@@ -12,6 +12,9 @@ import { translate } from "~/composables/usei18n";
 import { saveAsJpeg } from "save-html-as-image";
 // This is how import export default with a custom name
 import  locales from "~/constants/locales/stoleForm";
+import {toBlob} from "html-to-image";
+import {getStorage, ref as storageRef, uploadBytes} from "firebase/storage";
+import {tryCatch} from "standard-as-callback/built/utils";
 // also we can do this
 // import {default as locales} from "@/constants/locales/stoleForm"
 
@@ -31,7 +34,7 @@ const previewImageUrlRef2 = ref('')
 const showModalPreviewImage = ref(false)
 
 function saveImage(){
-  const node = document.getElementById("imageToSave2");
+  const node = document.getElementById("imageToSave");
   saveAsJpeg(node, { filename: "test", printDate: false });
 }
 
@@ -52,7 +55,7 @@ function handleChange (options: { file: UploadFileInfo, fileList: Array<UploadFi
 }
 
 // submit form
-const handleValidateClick = () => {
+const validateForm = () => {
   formRef.value?.validate((errors)=>{
     if(!errors){
       showModal.value = true
@@ -63,17 +66,25 @@ const handleValidateClick = () => {
 }
 
 // modal logic
- async function onPositiveClick () {
-  const orderUID = await submitOrder(orderForm.value)
-  stoleForm.value.orderUID = orderUID
+ async function onSubmitConfirm () {
+  try{
+    // upload order form and get the uid of the doc
+    const orderUID = await submitOrder(orderForm.value)
 
-   if(submitStole(stoleForm.value)){
+    // set the orderUID to stole.orderUID and upload stole form
+    stoleForm.value.orderUID = orderUID
+    await submitStole(stoleForm.value)
+
+    // upload stole image
+    await uploadStoleImage(orderUID, orderForm.value.schoolName)
+
+    // Reset forms, show success message and redirect the user
     store.resetForms()
     message.success('La orden se ha creado exitosamente.')
     navigateTo({path: '/',})
-  } else {
-     message.error('Something went wront uploading the order and stole form')
-   }
+  }catch(e) {
+    message.error('Something went wrong uploading the order and stole form', e)
+  }
 }
 
 function goBack(){
@@ -81,7 +92,21 @@ function goBack(){
     path: '/order/orderCreate',
   })
 }
-
+async function uploadStoleImage(orderUID: String, schoolName:String) {
+  const element = document.getElementById("imageToSave");
+  if(!element) return
+  try {
+    const blob = await toBlob(element);
+    const storage = getStorage()
+    const formattedSchoolName = schoolName.replace(/\s+/g, '_');
+    const testImageRef = storageRef(storage, `${formattedSchoolName}_${orderUID}_${+new Date}`)
+    // Upload the Blob file to Firebase Storage
+    const snapshot = await uploadBytes(testImageRef, blob);
+    console.log("Stole image uploaded successfully.", snapshot);
+  } catch (error) {
+    console.error("Something went wrong uploading the blob file of the stole image", error);
+  }
+}
 </script>
 
 <template>
@@ -117,19 +142,19 @@ function goBack(){
             </n-tooltip>
           </n-form-item>
 
-          <n-form-item label="Stole Color" path="color">
+          <n-form-item :label="t('stoleColor')" path="color">
             <n-select
                 v-model:value="stoleForm.color"
                 filterable
-                placeholder="You can start typing to search"
+                :placeholder="t('stoleColor')"
                 :options="stoleColorOptions"
                 clearable
             />
           </n-form-item>
 
-          <n-form-item label="Stole Lettering" path="lettering">
+          <n-form-item :label="t('stoleLettering')" path="lettering">
             <n-input v-model:value="stoleForm.lettering" clearable
-                     placeholder="Write the stole's letters Ex. ECHS"
+                     :placeholder="t('stoleLetteringPlaceholder')"
                      :maxlength="5"
             />
           </n-form-item>
@@ -144,7 +169,7 @@ function goBack(){
             />
           </n-form-item>
 
-          <n-form-item label="Color of Letters and Numbers" path="letteringAndNumberColors">
+          <n-form-item :label="t('colorLettersAndNumbers')" path="letteringAndNumberColors">
             <n-select
                 v-model:value="stoleForm.letteringAndNumberColors"
                 filterable
@@ -174,7 +199,7 @@ function goBack(){
 <!--            />-->
 <!--          </n-form-item>-->
 
-          <n-form-item label="upload photos">
+          <n-form-item :label="t('uploadPhotos')">
             <n-upload
                 action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
                 :default-upload="false"
@@ -197,28 +222,27 @@ function goBack(){
             <img :src="previewImageUrl" style="width: 100%" alt="uploaded image">
           </n-modal>
 
-
-          <n-space justify="center">
-            <n-form-item>
+          <n-space justify="center" gap-9>
               <n-button @click="goBack" mr-1>
-                Back
+                <NIcon size="18">
+                  <ArrowBack/>
+                </NIcon>
               </n-button>
-              <n-button @click="handleValidateClick">
-                Submit
+              <n-button @click="validateForm">
+                {{ t('submit') }}
               </n-button>
               <n-button @click="saveImage">
-                Save image
+                <NIcon mr-1.5><CloudDownloadOutline/></NIcon>
+                {{ t('download') }}
               </n-button>
-            </n-form-item>
           </n-space>
-
 
         </n-space>
       </n-form>
     </n-card>
 
     <StoleComponent
-        id="imageToSave2"
+        id="imageToSave"
         :stole-color="stoleForm.color"
         :lettering="stoleForm.lettering"
         :trim-color="stoleForm.borderColor"
@@ -236,7 +260,7 @@ function goBack(){
         content="Are you sure you want to submit?"
         positive-text="Confirm"
         negative-text="Cancel"
-        @positive-click="onPositiveClick"
+        @positive-click="onSubmitConfirm"
         @negative-click="showModal = false"
     />
   </div>
