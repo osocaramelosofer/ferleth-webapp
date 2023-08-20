5c1f9220a82bf423f9de8ae21ebd75737ade27ef
Fernando Miguel <miguel@itnora.com>
2023-08-20 13:21:13 -0600

    add function resertForms() and refactor the store

--- stores/storeOrder.ts
+++ stores/storeOrder.ts
@@ -6,20 +6,7 @@ import {Order, Stole} from "~/types/Order";
 export const useOrderStore = defineStore('order',() => {
   //In Setup Stores:
   // ref()s become state properties
-  const current = ref(1)
   const currentStatus = ref<StepsProps['status']>('process')
-
-  const formValueOrder: Ref<Order> = ref<Order>({
-    orderNumber: 0,
-    schoolName: '',
-    timestampCreation: +new Date,
-    dueTimestamp: +new Date,
-    tags: null,
-    pieces: null,
-    quality: 'medium',
-    costPerUnit: null,
-    totalCost: null
-  });
   const orderForm: Ref<Order> = ref<Order>({
     orderNumber: 0,
     schoolName: '',
@@ -46,13 +33,31 @@ export const useOrderStore = defineStore('order',() => {
   const doubleCount = computed(() => count.value * 2)
 
   // function()s become action
-  // maybe I will need this when I check the details of an order I will need to
-  // set details of the order you wanna see in some way so we can re-use the page orderCreate.vue to visualize the data
-  function setFormOrderValue(formValue: Order){
-    formValueOrder.value = formValue
+ function resetForms(){
+    orderForm.value = {
+      orderNumber: 0,
+      schoolName: '',
+      timestampCreation: +new Date,
+      dueTimestamp: +new Date,
+      tags: null,
+      pieces: null,
+      quality: 'medium',
+      costPerUnit: null,
+      totalCost: null
+    }
+    stoleForm.value = {
+      type: null,
+      color: null,
+      lettering: null,
+      borderColor: null,
+      letteringAndNumberColors: null,
+      logoColor1: null,
+      logoColor2: null,
+      orderUID:  null
+    }
   }
 
 
 
-  return {  formValueOrder, stoleForm, orderForm }
+  return { stoleForm, orderForm, resetForms }
 })
