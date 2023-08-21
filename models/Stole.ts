c6c1b809fa57a3600cb17e81dcd76b1347a29817
Fernando Miguel <miguel@itnora.com>
2023-08-20 20:49:21 -0600

    add Stole model and Interface

---
+++ models/Stole.ts
@@ -0,0 +1,23 @@
+import {StoleInterface} from "@/types/StoleInterface"
+export class Stole implements  StoleInterface {
+  type: String | null
+  color: String | null
+  lettering: String | null
+  borderColor: String | null
+  letteringAndNumberColors: String | null
+  logoColor1: String | null
+  logoColor2: String | null
+  orderUID: String | Number | null
+
+  constructor(type, color, lettering, borderColor, letteringAndNumberColors,
+              logoColor1, logoColor2, orderUID) {
+    this.type = type;
+    this.color = color;
+    this.lettering = lettering;
+    this.borderColor = borderColor;
+    this.letteringAndNumberColors = letteringAndNumberColors;
+    this.logoColor1 = logoColor1;
+    this.logoColor2 = logoColor2;
+    this.orderUID = orderUID;
+  }
+}
---
+++ types/StoleInterface.ts
@@ -0,0 +1,10 @@
+export interface StoleInterface {
+  type: String | null,
+  color: String | null,
+  lettering: String | null,
+  borderColor: String | null,
+  letteringAndNumberColors: String | null,
+  logoColor1: String | null,
+  logoColor2: String | null,
+  orderUID: String | Number | null
+}
